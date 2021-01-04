a1a528270b6bb07f2d173a2662796bfbf25bb08a
Karl Aleksander Leoste <KarlAleksander.Leoste@tptlive.ee>
2021-01-04 16:54:21 +0200

    first commit

---
+++ SoundVisualizer/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace SoundVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "WAV files|*.wav";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open wav";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(182, 34);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(951, 513);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(164, 472);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 42);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 559);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SoundVisualizer/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using NAudio.Wave;
+
+namespace SoundVisualizer
+{    
+    public partial class Form1 : Form
+    {
+        private int samplerate = 0;
+        private int channels = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //copypaste from stackoverflow A
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = openFileDialog1.FileName;
+                //DoThing();
+                DoBetterThing();
+            }
+        }
+                
+        private void DoBetterThing()
+        {
+            List<float[]> buffers = new List<float[]>();
+
+            using (WaveFileReader reader = new WaveFileReader(label1.Text))
+            {
+                ISampleProvider provider = reader.ToSampleProvider();
+                samplerate = provider.WaveFormat.SampleRate;
+                channels = provider.WaveFormat.Channels;
+
+                int seconds = (int)numericUpDown1.Value;
+                int takes = (int)(reader.TotalTime.TotalSeconds / seconds);
+                int count = samplerate * seconds * channels;
+                int countp2 = count + 2; // bad variable name ik
+
+                for (int i = 0; i < takes; i++)
+                {
+                    float[] buffer = new float[countp2];
+
+                    //read additional 2 floats then scroll back a bit
+                    provider.Read(buffer, 0, countp2);
+                    reader.Seek(-2 * reader.BlockAlign, SeekOrigin.Current);
+
+                    Fourier.ForwardReal(buffer, count);
+                    buffers.Add(buffer);
+                }
+            }
+
+            listBox1.BeginUpdate();
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(buffers.ToArray());
+            listBox1.EndUpdate();
+        }
+
+        private void SetGraph(float[] numbers)
+        {            
+            chart1.Series.Clear();
+            chart1.Series.Add(numbers.GetHashCode().ToString());
+            System.Windows.Forms.DataVisualization.Charting.Series series = chart1.Series.FindByName(numbers.GetHashCode().ToString());
+
+            //series.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline; // to get connected line            
+
+            int count = Math.Min(numbers.Length, 4000);
+            for (int i = 0; i < count; i++)
+            {
+                int index = series.Points.AddXY(i, Math.Abs(numbers[i]));
+                DataPoint point = series.Points.ElementAt(index);
+
+                double distance = 0;
+                if (i > 0) distance = GetNoteDistance(440, i);
+                double thingy = distance % 12 / 12;
+                if (thingy < 0) thingy = 1 + thingy;                
+                int value = (int)(256 * thingy);
+                point.Color = Color.FromArgb(value, 0, 0);
+
+                //series.Points.ElementAt(index).Color = Color.FromArgb(i % 256, 255 - i % 256, i * i % 256);
+                //series.Points.AddXY(i, men[i]); // this is pure wav file
+            }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float[] data = listBox1.SelectedItem as float[];
+            SetGraph(data);
+
+            WaveFormat format = WaveFormat.CreateIeeeFloatWaveFormat(samplerate, channels);
+
+            float[] clone = (data.Clone() as float[]).ToList().Append(0).Append(0).ToArray();
+            Fourier.InverseReal(clone, data.Length);
+            byte[] frames = clone.SelectMany(value => BitConverter.GetBytes(value)).ToArray();
+            IWaveProvider provider = new RawSourceWaveStream(new MemoryStream(frames), format);
+            WaveOut wo = new WaveOut();
+            wo.Init(provider);
+            wo.Play();
+        }
+
+        private double GetNoteDistance(int baseFrequency, int comparedFrequency)
+        {
+            double result;
+
+            double divided = comparedFrequency / (double)baseFrequency;
+            double logged = Math.Log(divided, 2);
+            result = logged * 12;
+
+            return result;
+        }
+    }
+}

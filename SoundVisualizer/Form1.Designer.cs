7d6f64c267d254fca48f1425b8dfd25e90ae0486
Karl Aleksander Leoste <KarlAleksander.Leoste@tptlive.ee>
2021-01-04 22:33:55 +0200

    basic video render ASL

--- SoundVisualizer/Form1.Designer.cs
+++ SoundVisualizer/Form1.Designer.cs
@@ -29,9 +29,9 @@ namespace SoundVisualizer
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,6 +41,8 @@ namespace SoundVisualizer
             this.label2 = new System.Windows.Forms.Label();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
@@ -72,26 +74,26 @@ namespace SoundVisualizer
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
             this.chart1.Location = new System.Drawing.Point(182, 43);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(951, 504);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1105, 504);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "chart1";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 108);
+            this.listBox1.Location = new System.Drawing.Point(12, 64);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(164, 433);
+            this.listBox1.Size = new System.Drawing.Size(164, 303);
             this.listBox1.TabIndex = 3;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -112,9 +114,9 @@ namespace SoundVisualizer
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(93, 14);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "seconds";
+            this.label2.Text = "framerate";
             // 
             // numericUpDown2
             // 
@@ -142,11 +144,22 @@ namespace SoundVisualizer
             this.label3.TabIndex = 7;
             this.label3.Text = "columns";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Save video";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1145, 559);
+            this.ClientSize = new System.Drawing.Size(1299, 559);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.label2);
@@ -176,6 +189,8 @@ namespace SoundVisualizer
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- SoundVisualizer/Form1.cs
+++ SoundVisualizer/Form1.cs
@@ -13,6 +13,10 @@ using MathNet.Numerics;
 using MathNet.Numerics.IntegralTransforms;
 using NAudio.Wave;
 using ColorMine.ColorSpaces;
+using Splicer.Timeline;
+using Splicer.Renderer;
+using Splicer.WindowsMedia;
+//using Accord.Video.FFMPEG;
 
 namespace SoundVisualizer
 {    
@@ -20,7 +24,9 @@ namespace SoundVisualizer
     {
         private int samplerate = 0;
         private int channels = 0;
-        private int seconds = 0;
+        private int count = 0;
+        private double seconds = 0;
+        private double length = 0;
 
         public Form1()
         {
@@ -48,18 +54,20 @@ namespace SoundVisualizer
                 ISampleProvider provider = reader.ToSampleProvider();
                 samplerate = provider.WaveFormat.SampleRate;
                 channels = provider.WaveFormat.Channels;
+                length = reader.TotalTime.TotalSeconds;
 
-                seconds = (int)numericUpDown1.Value;
+                seconds = 1 / (double)numericUpDown1.Value;
                 int takes = (int)(reader.TotalTime.TotalSeconds / seconds);
-                int count = samplerate * seconds * channels;
+                int count = this.count = (int)(samplerate * seconds * channels);
                 int countp2 = count + 2; // bad variable name ik
+                int buffercount = Math.Max(samplerate, count) + 2;
 
                 for (int i = 0; i < takes; i++)
                 {
-                    float[] buffer = new float[countp2];
+                    float[] buffer = new float[buffercount];
 
                     //read additional 2 floats then scroll back a bit
-                    provider.Read(buffer, 0, countp2);
+                    provider.Read(buffer, 0, countp2);                    
                     reader.Seek(-2 * reader.BlockAlign, SeekOrigin.Current);
 
                     Fourier.ForwardReal(buffer, count);
@@ -84,19 +92,29 @@ namespace SoundVisualizer
             int count = Math.Min(numbers.Length, (int)numericUpDown2.Value);            
             for (int i = 0; i < count; i++)
             {
+                //math.max is to prevent 0. sideeffect is 0 and 1 have same distance but who cares                
+                double distance = GetNoteDistance(440, Math.Max(i, 1));
+
                 double number = 0;
-                for (int k = 0; k < seconds; k++)
+                if (seconds > 1)
                 {
-                    number += Math.Abs(numbers[i * seconds + k]);
+                    for (int k = 0; k < (int)seconds; k++)
+                    {
+                        number += Math.Abs(numbers[(int)(i * seconds + k)]);
+                    }
+                    number = number / seconds;
                 }
-                number = number / seconds;
-                int index = series.Points.AddXY(i, number);
+                else
+                {
+                    number = Math.Abs(numbers[(int)(i * seconds)]);
+                }
+                
+                //AddXy(i) shows frequency. AddXy(distance) shows notes. higher notes have more data points cause how it works lol
+                int index = series.Points.AddXY(distance, number);
                 DataPoint point = series.Points.ElementAt(index);
 
                 Color c = Color.FromArgb(0);
-
-                double distance = 0;
-                if (i > 0) distance = GetNoteDistance(440, i);
+                
                 double thingy = distance % 12 / 12;
                 if (thingy < 0) thingy = 1 + thingy;
                 Hsv hsv = new Hsv();
@@ -109,7 +127,16 @@ namespace SoundVisualizer
                 //series.Points.ElementAt(index).Color = Color.FromArgb(i % 256, 255 - i % 256, i * i % 256);
                 //series.Points.AddXY(i, men[i]); // this is pure wav file
             }
-            
+        }
+
+        private byte[] SpectrumToAudioFrames(float[] data)
+        {
+            int amount = Math.Min(count + 2, data.Length);
+            float[] clone = new float[amount];
+            Buffer.BlockCopy(data, 0, clone, 0, amount);
+            Fourier.InverseReal(clone, clone.Length - 2);
+            byte[] frames = clone.SelectMany(value => BitConverter.GetBytes(value)).ToArray();
+            return frames;
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -117,11 +144,8 @@ namespace SoundVisualizer
             float[] data = listBox1.SelectedItem as float[];
             SetGraph(data);
 
-            WaveFormat format = WaveFormat.CreateIeeeFloatWaveFormat(samplerate, channels);
-
-            float[] clone = (data.Clone() as float[]).ToList().Append(0).Append(0).ToArray();
-            Fourier.InverseReal(clone, data.Length);
-            byte[] frames = clone.SelectMany(value => BitConverter.GetBytes(value)).ToArray();
+            WaveFormat format = WaveFormat.CreateIeeeFloatWaveFormat(samplerate, channels);                        
+            byte[] frames = SpectrumToAudioFrames(data);
             IWaveProvider provider = new RawSourceWaveStream(new MemoryStream(frames), format);
             WaveOut wo = new WaveOut();
             wo.Init(provider);
@@ -137,6 +161,58 @@ namespace SoundVisualizer
             result = logged * 12;
 
             return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                int width = 1280;
+                int height = 720;
+                int framerate = (int)numericUpDown1.Value;
+
+                using (ITimeline timeline = new DefaultTimeline(framerate))
+                {
+                    IGroup group = timeline.AddVideoGroup(32, width, height);
+                    ITrack videoTrack = group.AddTrack();
+                    IGroup audio = timeline.AddAudioGroup();
+                    ITrack audioTrack = audio.AddTrack();
+
+                    int i = 0;
+                    double miniDuration = 1.0 / framerate;
+
+                    foreach (float[] item in listBox1.Items)
+                    {
+                        SetGraph(item);
+                        Bitmap image = new Bitmap(width, height);
+                        chart1.DrawToBitmap(image, new Rectangle(0, 0, width, height));
+
+                        videoTrack.AddImage(image, 0, i * miniDuration, (i + 1) * miniDuration);                        
+                        i++;
+                    }
+
+                    audioTrack.AddAudio(label1.Text, InsertPosition.Absolute, 0, 0, length);
+
+                    IRenderer renderer = new WindowsMediaRenderer(timeline, saveFileDialog1.FileName, WindowsMediaProfiles.HighQualityVideo);
+                    renderer.Render();
+                }
+
+                /*using (var vFWriter = new VideoFileWriter())
+                {
+                    vFWriter.Open(saveFileDialog1.FileName, width, height, framerate, VideoCodec.Raw);
+
+                    foreach (float[] item in listBox1.Items)
+                    {
+                        SetGraph(item);
+                        Bitmap image = new Bitmap(width, height);
+                        chart1.DrawToBitmap(image, new Rectangle(0, 0, width, height));
+                        vFWriter.WriteVideoFrame(image);
+                        vFWriter.WriteAudioFrame(SpectrumToAudioFrames(item));                        
+                    }
+                                        
+                    vFWriter.Close();
+                }*/
+            }
         }
     }
 }
